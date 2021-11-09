061d77af847b37a252c5c3bbf43ec56ec3cb4b18
zackartborges <zackartborges@gmail.com>
2021-11-09 14:03:42 -0500

    adds input component and props spread syntax within component.

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,8 +1,9 @@
 import React from "react";
-import { View, Text, StyleSheet, TextInput, Button } from "react-native";
+import { View, Text, StyleSheet, Button } from "react-native";
 
 import Card from "../components/Card";
-import Colors from "../constants/colors"
+import Colors from "../constants/colors";
+import Input from "../components/Input";
 
 const StartGameScreen = (props) => {
   return (
@@ -10,7 +11,14 @@ const StartGameScreen = (props) => {
       <Text style={styles.title}>Start a New Game!</Text>
       <Card style={styles.inputContainer}>
         <Text>Select a Number</Text>
-        <TextInput />
+        <Input
+          style={styles.input}
+          blurOnSubmit
+          autoCapitalize="none"
+          autoCorrect={false}
+          keyboardType="number-pad"
+          maxLength={2}
+        />
         <View style={styles.buttonContainer}>
           <View style={styles.button}>
             <Button title="Reset" onPress={() => {}} color={Colors.accent} />
@@ -47,7 +55,11 @@ const styles = StyleSheet.create({
   },
   button: {
     width: 100,
-  }
+  },
+  input: {
+    width: 50,
+    textAlign: "center",
+  },
 });
 
 export default StartGameScreen;
