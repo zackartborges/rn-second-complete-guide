a468b495217e0cb9f336fa74743203769ebe5b68
zackartborges <zackartborges@gmail.com>
2021-11-09 12:57:49 -0500

    adds start game screen, adds card.js component and nice shadow styles

--- App.js
+++ App.js
@@ -1,19 +1,20 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
 
-import Header from "./components/Header";
+import Header from './components/Header';
+import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
   return (
     <View style={styles.screen}>
-      <Header title="Guess A Number" />
+      <Header title="Guess a Number" />
+      <StartGameScreen />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
+  screen: {
+    flex: 1
+  }
 });
